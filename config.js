aa41ccb801c17750bcf0ab626df19c15702f6d39
mailforsachin <mailforsachin@gmail.com>
2019-03-15 21:03:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-    consumer_key:         'QP0O9ffZcTzWOHeVQ66I2NFzG',
-    consumer_secret:      'Vw1F2vFugpaRU82DRE8eKZvpGgpXplkUBa9Zjr5ygSgVNcmbWj',
-    access_token:         '2443165910-nmmpALlN5Pd3WjkurrhMhB0U1oT9le9BZmMj5xE',
-    access_token_secret:  'RioTzCgCmjz47Ez8AEZ4vM8Mf9UiBcC126FW7EynJoB1Q'
+      consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
   }
